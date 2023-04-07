8960169e933e1d475eeb1af381bd75f176243042
lishiyu <522583971@qq.com>
2023-04-07 11:00:43 -0400

    add

--- save.sh
+++ save.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 cp /etc/clickhouse-server/custom_function.xml /home/lishiyu/talib_test/data_process/sql_func/custom_function.xml
 cp /etc/clickhouse-server/custom_function.xml /home/lishiyu/talib_test/data_process/sql_func/custom_function.xml
 git add *
