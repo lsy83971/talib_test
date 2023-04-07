056c5d71ed6db06d319bf9bd74a768d51dee1ca7
lishiyu <522583971@qq.com>
2023-04-07 11:06:43 -0400

    add

--- save.sh
+++ save.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cp -f /etc/clickhouse-server/custom_function.xml /home/lishiyu/talib_test/data_process/sql_func/custom_function.xml
-cp -f /etc/clickhouse-server/custom_function.xml /home/lishiyu/talib_test/data_process/sql_func/custom_function.xml
+cp -f /etc/clickhouse-server/custom_function.xml /home/lishiyu/talib_test/data_process/sql_func/
 git add *
 git commit -m 'add'
 git push'
