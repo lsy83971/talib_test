ae72b6d275534f596a89fdf887101179f0c71d89
lishiyu <522583971@qq.com>
2023-04-08 00:23:15 -0400

    commit

--- save.sh
+++ save.sh
@@ -4,5 +4,5 @@ rm -f "/home/lishiyu/talib_test/data_process/sql_func/lsy_exch_detail.py"
 cp "/etc/clickhouse-server/custom_function.xml" "/home/lishiyu/talib_test/data_process/sql_func/"
 cp "/var/lib/clickhouse/user_scripts/lsy_exch_detail.py" "/home/lishiyu/talib_test/data_process/sql_func/"
 git add *
-git commit -m 'add'
+git commit -m 'commit'
 git push
